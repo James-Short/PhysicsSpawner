03664385d5d171c500fc5426bb0f10c66aadf0e5
James-Short <143297015+James-Short@users.noreply.github.com>
2025-03-31 14:57:24 -0500

    Add files via upload
    This is just some code to get a basic setup done.

---
+++ ball.py
@@ -0,0 +1,54 @@
+
+class Ball:
+    def __init__(self, x, y, radius):
+        import random
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.vx = random.randint(-15, 15)
+        self.vy = 0
+    
+    def update(self):
+        self.vy += 0.5
+        self.y += self.vy
+        
+        self.x += self.vx
+        
+        
+        if self.y + self.radius >= 600:
+            self.y = 600 - self.radius
+            
+            if abs(self.vy) < 1:
+                self.vy = 0
+            else:
+                self.vy *= -0.8
+        
+        elif self.y - self.radius <= 0:
+            self.y = self.radius
+            
+
+            if abs(self.vy) < 1:
+                self.vy = 0
+            else:
+                self.vy *= -0.8
+        
+        if self.x + self.radius >= 600:
+            self.x = 600 - self.radius
+            
+            if abs(self.vx) < 1:
+                self.vx = 0
+            else:
+                self.vx *= -0.8
+        
+        elif self.x - self.radius <= 0:
+            self.x = self.radius
+            
+            if abs(self.vx) < 1:
+                self.vx = 0
+            else:
+                self.vx *= -0.8
+    
+    def draw(self, screen):
+        import pygame
+        pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.radius)
+        
---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from ball import Ball
+
+#Basic screen setup
+
+screen = pygame.display.set_mode((600, 600))
+black = (0, 0, 0)
+clock = pygame.time.Clock()
+
+#Utilizing balls
+
+balls = []
+
+balls.append(Ball(300, 0, 25))
+
+running = True
+
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill((0, 0, 0))
+    
+    for ball in balls:
+        ball.update()
+        ball.draw(screen)
+    
+    
+    pygame.display.flip()
+    clock.tick(50)
+    
